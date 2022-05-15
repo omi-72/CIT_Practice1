00c1291da4dbd650c0caf9e853ef566aa6a64957
omi-72 <jannat.omi722@gmail.com>
2022-05-15 20:36:17 +0600

    set Toast massage

--- app/src/main/java/com/example/amrich221/MainActivity.java
+++ app/src/main/java/com/example/amrich221/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.amrich221;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn=findViewById(R.id.btn_rich);
+
+        btn.setOnClickListener(V ->{
+            Toast.makeText(this,"Button Clicked !", Toast.LENGTH_SHORT).show();
+        });
+
+
     }
 }
